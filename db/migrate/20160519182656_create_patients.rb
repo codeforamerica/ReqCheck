a4ca71de30b64e8da230b32fdc0ca39d5044aaa1
Kevin B <kberry@codeforamerica.org>
2016-05-19 12:15:29 -0700

    created patient migration file

--- db/migrate/20160519182656_create_patients.rb
+++ db/migrate/20160519182656_create_patients.rb
@@ -1,8 +1,23 @@
 class CreatePatients < ActiveRecord::Migration
   def change
     create_table :patients do |t|
+      t.integer   :patient_no, null: false
+      t.date      :dob, null: false
+      t.string    :first_name, null: false
+      t.string    :last_name, null: false
+      t.string    :address
+      t.string    :address2
+      t.string    :city
+      t.string    :state
+      t.string    :zip_code
+      t.string    :cell_phone
+      t.string    :home_phone
+      t.string    :race
+      t.string    :ethnicity
 
       t.timestamps null: false
     end
+    add_index :patients, :patient_no
+    add_index :patients, :last_name
   end
 end
