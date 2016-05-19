8482ffd4d32cb1d7048a4c77e860701695e21ea0
Kevin B <kberry@codeforamerica.org>
2016-05-19 16:28:00 -0700

    updated new schema file and routes after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160519213236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "immunizations", force: :cascade do |t|
+    t.string   "vaccine_code",                    null: false
+    t.string   "patient_no",                      null: false
+    t.date     "imm_date",                        null: false
+    t.boolean  "send_flag"
+    t.boolean  "history_flag",    default: false, null: false
+    t.string   "provider_code"
+    t.string   "cosite"
+    t.string   "region"
+    t.string   "dosage"
+    t.string   "manufacturer"
+    t.string   "lot_no"
+    t.date     "expiration_date"
+    t.string   "dose_no"
+    t.string   "encounter_no"
+    t.date     "sent_date"
+    t.string   "vfc_code"
+    t.integer  "facility_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  create_table "patient_profiles", force: :cascade do |t|
+    t.integer "patient_id",    null: false
+    t.integer "record_number", null: false
+    t.date    "dob",           null: false
+    t.string  "address"
+    t.string  "address2"
+    t.string  "city"
+    t.string  "state"
+    t.string  "zip_code"
+    t.string  "cell_phone"
+    t.string  "home_phone"
+    t.string  "race"
+    t.string  "ethnicity"
+  end
+
+  add_index "patient_profiles", ["patient_id"], name: "index_patient_profiles_on_patient_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
