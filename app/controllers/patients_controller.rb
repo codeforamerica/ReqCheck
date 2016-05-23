acdddb2856dd1b61b83006ebc4799409d122efaa
Kevin B <kberry@codeforamerica.org>
2016-05-23 13:27:32 -0700

    updated search and results

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,15 @@
 class PatientsController < InheritedResources::Base
 
+  def index
+    @patients = Patient.all
+    if params[:search] != ''
+      @patients = Patient.joins(:patient_profile).where(patient_profiles: {record_number: params[:search]}).order("created_at DESC")
+    else
+      @patients = Patient.all.order('created_at DESC')
+    end
+  end
+
+
   private
 
     def patient_params
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < User
   has_one :patient_profile
-  delegate :dob, to: :patient_profile
+  delegate :dob, :record_number, :address, :address2, :city, :state, :zip_code, :cell_phone,
+    :home_phone, :race, :ethnicity, to: :patient_profile
 end
+
+      
