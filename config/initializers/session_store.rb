014efcec37bf39134b12d650f1268b79d90f8290
Kevin B <kberry@codeforamerica.org>
2016-06-08 16:36:35 -0700

    removed shotbot and included recordcheck

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_shotbot_session'
+Rails.application.config.session_store :cookie_store, key: '_recordcheck_session'
