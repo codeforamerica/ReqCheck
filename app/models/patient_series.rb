e6b2835a33fc486c8dfa9cbd6ea129ef29a3cb70
Kevin B <kberry@codeforamerica.org>
2016-07-26 17:27:57 -0700

    created the beginning for a patient series and target doses

--- app/models/patient_series.rb
+++ app/models/patient_series.rb
@@ -1,3 +1,10 @@
 class PatientSeries
   include ActiveModel::Model
+
+  attr_accessor :patient, :antigen_series
+
+  def initialize(patient:, antigen_series:)
+    @patient        = patient
+    @antigen_series = antigen_series
+  end
 end
--- spec/models/patient_series_spec.rb
+++ spec/models/patient_series_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PatientSeries, type: :model do
+  let(:test_patient) { FactoryGirl.create(:patient) }
+  let(:antigen_series) { FactoryGirl.create(:antigen_series) }
+  
   describe 'validations' do
+    it 'takes a patient and antigen_series as parameters' do
+      expect(
+        PatientSeries.new(antigen_series: antigen_series,
+                       patient: test_patient).class.name
+      ).to eq('PatientSeries')
+    end
+
+    it 'requires a patient object' do
+      expect{PatientSeries.new(antigen_series: antigen_series)}.to raise_exception
+    end
+    it 'requires an antigen_series' do
+      expect{PatientSeries.new(patient: test_patient)}.to raise_exception
+    end
+  end
+
+  describe '#create_target_doses' do
+    it 'maps through the antigen_series_doses and creates a target_dose for each one' do
+      patient_series = PatientSeries.new(antigen_series: antigen_series, patient: test_patient)
+      antigen_series_length = antigen_series.length
+      patient_series.create_target_doses(antigen_series.doses)
+      expect(patient_series.target_doses.length).to eq(antigen_series_length)
+    end
   end
 end
