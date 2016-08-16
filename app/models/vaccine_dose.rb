cccab23bcd879b37c23e46c6c229ff6de5b7b904
Kevin B <kberry@codeforamerica.org>
2016-08-15 18:32:18 -0700

    updated the VaccineDose to include the timecalc module

--- app/models/vaccine_dose.rb
+++ app/models/vaccine_dose.rb
@@ -2,16 +2,18 @@ class VaccineDose < ActiveRecord::Base
   belongs_to :patient_profile
   has_one :patient, through: :patient_profile
 
+  include TimeCalc
+
   def patient_age_at_vaccine_dose
     unless self.patient.nil?
-      TimeCalc.detailed_date_diff(self.patient.dob, self.date_administered)
+      detailed_date_diff(self.patient.dob, self.date_administered)
     else
       'N/A'
     end
   end
 
   def time_since_vaccine_dose
-    TimeCalc.detailed_date_diff(self.date_administered)
+    detailed_date_diff(self.date_administered)
   end
 
   def vaccine_info
