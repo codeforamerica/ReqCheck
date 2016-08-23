0ac3b19f7094282faa841302e3634489cc43c110
Kevin B <kberry@codeforamerica.org>
2016-08-22 18:14:44 -0700

    removed extraneous self in the ruby class

--- app/models/antigen_series_dose.rb
+++ app/models/antigen_series_dose.rb
@@ -2,23 +2,24 @@ class AntigenSeriesDose < ActiveRecord::Base
   belongs_to :antigen_series
   has_one :conditional_skip
   has_many :intervals
-  has_and_belongs_to_many :dose_vaccines, class_name: AntigenSeriesDoseVaccine,
-    join_table: :antigen_series_doses_to_vaccines
+  has_and_belongs_to_many :dose_vaccines,
+                          class_name: AntigenSeriesDoseVaccine,
+                          join_table: :antigen_series_doses_to_vaccines
   validates :dose_number, presence: true
 
   def allowable_vaccines
-    self.dose_vaccines.select { |vaccine| vaccine.preferable == false }
+    dose_vaccines.select { |vaccine| vaccine.preferable == false }
   end
 
   def preferable_vaccines
-    self.dose_vaccines.select { |vaccine| vaccine.preferable == true }
+    dose_vaccines.select { |vaccine| vaccine.preferable == true }
   end
 
   def allowable_intervals
-    self.intervals.select { |interval| interval.allowable == true }
+    intervals.select { |interval| interval.allowable == true }
   end
 
   def preferable_intervals
-    self.intervals.select { |interval| interval.allowable == false }
+    intervals.select { |interval| interval.allowable == false }
   end
 end
