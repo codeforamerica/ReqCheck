3860ca04645a5955d43bc89ac70a6c04e0933bbe
Kevin B <kberry@codeforamerica.org>
2016-08-24 17:01:07 -0700

    added gender to the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,26 @@
 require_relative '../spec/support/vax_codes'
 
 patients_list = [
-  [ "Kevin", "Berry", "02/08/1990"],
-  [ "James", "Halstead", "03/12/1989"],
-  [ "Coco", "Crisp", "04/22/1990"],
-  [ "OilCan", "Boyd", "05/18/1985"],
-  [ "Nomar", "Garciaparra", "06/15/1992"],
-  [ "Manny", "Ramirez", "12/07/1978"],
-  [ "David", "Ortiz", "11/01/1981"],
-  [ "Mo", "Vaughn", "10/28/1988"],
-  [ "Trot", "Nixon", "09/26/1994"],
-  [ "Tim", "Wakefield", "06/24/1998"],
-  [ "Dustin", "Pedroia", "08/13/2000"],
+  ['Jared', 'Leto', '02/08/1990', 'M'],
+  ['James', 'Halstead', '03/12/1989', 'M'],
+  ['Coco', 'Crisp', '04/22/1990', 'M'],
+  ['OilCan', 'Boyd', '05/18/1985', 'M'],
+  ['Nomar', 'Garciaparra', '06/15/1992', 'M'],
+  ['Manny', 'Ramirez', '12/07/1978', 'M'],
+  ['Leslie', 'Ortiz', '11/01/1981', 'F'],
+  ['Jane', 'Vaughn', '10/28/1988', 'F'],
+  ['Samantha', 'Nixon', '09/26/1994', 'F'],
+  ['Laura', 'Wakefield', '06/24/1998', 'F'],
+  ['Nuria', 'Pedroia', '08/13/2000', 'F']
 ]
 
 patients_list.each_with_index do |value, index|
-  patient = Patient.create( first_name: value[0], last_name: value[1],
+  patient = Patient.create(
+    first_name: value[0], last_name: value[1],
     email: "#{value[1]}#{index.to_s}@example.com",
     patient_profile_attributes: {
-      dob: Date.strptime(value[2], '%m/%d/%Y'), record_number: (index + 1)
+      dob: Date.strptime(value[2], '%m/%d/%Y'), record_number: (index + 1),
+      gender: value[3]
     }
   )
   vaccine_types = ["MCV6", "DTaP", "MMR9", "HepB"]
