81f1e39bb902ae76e8684b6dd7f3d1e0536947c3
Kevin Berry <berry.kevinw@gmail.com>
2016-09-13 14:44:53 -0700

    starting point for KCMO HD tests

---
+++ spec/support/kcmo_data.rb
@@ -0,0 +1,35 @@
+
+
+
+
+patient_1 = {
+  patient_number: 1,
+  patient_name: 'Test Patient',
+  dob: '10/15/2015',
+  vaccines: [
+    {
+      cvx_code: 110,
+      description: 'C-DTaP/Hep B/IPV',
+      date_administered: '07/19/2016',
+      history: false
+    },
+    {
+      cvx_code: 8,
+      description: 'C-Hepatitis B Vaccine',
+      date_administered: '07/19/2016',
+      history: true
+    },
+    {
+      cvx_code: 49,
+      description: 'C-HIB',
+      date_administered: '07/19/2016',
+      history: false
+    },
+    {
+      cvx_code: 133,
+      description: 'C-Pneumococcal Conjugate PCV13',
+      date_administered: '07/19/2016',
+      history: false
+    }
+  ]
+}
