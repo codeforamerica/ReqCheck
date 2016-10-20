a588c927c1c04ace2c2a298d613c8a8df482fcd5
Kevin B <kberry@codeforamerica.org>
2016-10-19 22:41:48 -0700

    updated gemfile and the models to ensure security

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :recoverable, :trackable,
-         :timeoutable, :lockable
+  devise :database_authenticatable, :recoverable, :trackable, :lockable,
+         :timeoutable
 
   def role?(r)
     role.present? && role.include?(r.to_s)
