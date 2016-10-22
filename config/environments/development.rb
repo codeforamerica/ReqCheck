c12caaeb646af3f0d3825b01ff2bba0cccca7028
Kevin B <kberry@codeforamerica.org>
2016-10-22 14:08:01 -0700

    added extractor env variables to development setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,3 +45,6 @@ Rails.application.configure do
   # For Devise during development
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+ENV['EXTRACTOR_NAME'] = 'test'
+ENV['EXTRACTOR_PASSWORD'] = 'test'
