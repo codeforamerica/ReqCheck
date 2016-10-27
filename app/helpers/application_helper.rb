95f0ef828b0ffb780c31caa3ba75d3f471d65c15
Kevin B <kberry@codeforamerica.org>
2016-10-26 21:49:55 -0500

    fixed errors on patients record view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def patient_record_vaccine_groups
+    ['Diptheria, Tetanus, Pertussis', 'HIB', 'Hepatitis A',
+     'Hepatitis B', 'Human Papillomavirus', 'Measles, Mumps, Rubella',
+     'Meningococcal', 'Pneumococcal', 'Polio', 'Rotavirus', 'Varicella',
+     'Influenza', 'Travel & Other']
+  end
+
+
 end
